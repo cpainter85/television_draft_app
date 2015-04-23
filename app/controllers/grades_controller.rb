0e234952e29d3880dd28b86d5dfc0dfc9411ef96
Charles Painter <cpainter85@gmail.com>
2015-04-22 20:04:00 -0600

    grade index and ability to add/edit grade

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -1,6 +1,23 @@
 class GradesController < ApplicationController
+
   def index
+    @users = User.all
     @picks = Pick.all
     @round = 1
+  end
+
+  def edit
+    @pick = Pick.find(params[:pick_id])
+    @grade = @pick.grades.find(params[:id])
+  end
+
+  def update
+    @pick = Pick.find(params[:pick_id])
+    @grade = @pick.grades.find(params[:id])
+    if @grade.update(params.require(:grade).permit(:grade))
+      redirect_to grades_path, notice: "Grade for #{@pick.name} successfully submited!"
+    else
+      render :edit
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
     resources :picks, only: [:new, :create, :edit, :update]
   end
 
+  resources :picks, only: [:show] do
+    resources :grades, only: [:edit, :update]
+  end
+
   resources :grades, only: [:index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
